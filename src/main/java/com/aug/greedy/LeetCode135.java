40752219d564fc25c3d2ef1d60bbeff060bf6fa4
LD <767936541@qq.com>
2024-04-09 15:28:25 +0800

    training

--- src/main/java/com/aug/greedy/LeetCode135.java
+++ src/main/java/com/aug/greedy/LeetCode135.java
@@ -1,7 +1,28 @@
 package com.aug.greedy;
 
+import java.util.Arrays;
+
 public class LeetCode135 {
     public int candy(int[] ratings) {
+        int sum = 0;
+        int[] candyNum = new int[ratings.length];
+        Arrays.fill(candyNum,1);
 
+        for (int i = 1; i < ratings.length; i++) {
+            if(ratings[i] > ratings[i-1]){
+                candyNum[i] = candyNum[i-1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1] && candyNum[i] <= candyNum[i+1]){
+                candyNum[i] = candyNum[i+1] + 1;
+            }
+        }
+
+        for (int i : candyNum) {
+            sum += i;
+        }
+        return sum;
     }
 }
